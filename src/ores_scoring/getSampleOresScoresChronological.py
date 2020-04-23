4d736c180d024cd1b157d9694dd145bf05e26389
Zachary Levonian <levon003@umn.edu>
2020-04-22 22:45:59 -0500

    Added fork of sample scoring script to avoid making redundant requests and to append to the score CSV.

---
+++ src/ores_scoring/getSampleOresScoresChronological.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# Script to retreive ORES scores for a sample
+
+import mwapi
+import mwxml
+import mwxml.utilities
+import mwcli
+import oresapi
+
+import numpy as np
+import pandas as pd
+import os
+from tqdm import tqdm
+import bz2
+import gzip
+import json
+import re
+import hashlib
+from datetime import datetime
+import nltk
+import scipy.stats
+import para
+from itertools import groupby
+from collections import Counter
+import pickle
+
+
+def main():
+    # set directories
+    git_root_dir = '/export/scratch2/levon003/repos/wiki-ores-feedback'
+    derived_data_dir = os.path.join(git_root_dir, "data", "derived")
+    working_dir = os.path.join(derived_data_dir, 'revision_sample')
+    
+    # read in the sample dataframe
+    revision_sample_dir = os.path.join(derived_data_dir, 'revision_sample')
+    sample_filepath = os.path.join(revision_sample_dir, 'sample3_all.pkl')
+    rev_df = pd.read_pickle(sample_filepath)
+    
+    # read in the ORES scores
+    ores_dfs = []
+    for fname in ['sample1_ores_scores.csv', 'sample2_ores_scores.csv', 'sample3_ores_scores.csv']:
+        sample_filepath = os.path.join(revision_sample_dir, fname)
+        ores_dfs.append(pd.read_csv(sample_filepath, header=None, names=['rev_id', 'damaging_prob', 'damaging_pred', 'goodfaith_prob', 'goodfaith_pred']))
+    ores_df = pd.concat(ores_dfs, axis=0)
+    # remove invalid scores
+    ores_df = ores_df[(ores_df.damaging_prob != -1)&(ores_df.goodfaith_prob != -1)]
+    # remove duplicates
+    ores_df = ores_df.drop_duplicates(subset='rev_id')
+    print(f"Found {len(ores_df)} existing ORES scores.")
+    
+    # remove scored revs from the rev_df
+    revs_with_ores_scores = set(ores_df.rev_id)
+    rev_df = rev_df[~rev_df.rev_id.isin(revs_with_ores_scores)]
+    
+    # sort chronologically
+    rev_df = rev_df.sort_values(by='rev_timestamp')
+    
+    # extract the revision ids to retrieve
+    rev_id_list = rev_df.rev_id
+    print(f"Attempting to retrieve ORES scores for {len(rev_id_list)} rev_ids.")
+    
+    # establish the session and create the score iterator
+    session = oresapi.Session("https://ores.wikimedia.org", 
+                              user_agent="levon003@umn.edu - ores.api for UMN research",
+                              parallel_requests=2, batch_size=50)
+    scores = session.score("enwiki", ["damaging", "goodfaith"], rev_id_list)
+    
+    scores_list = []
+    ores_score_filepath = os.path.join(working_dir, 'sample3_ores_scores.csv')
+    with open(ores_score_filepath, 'a') as outfile:
+        total_processed = 0
+        for rev_id, score in tqdm(zip(rev_id_list, scores), total=len(rev_id_list)):
+            score['rev_id'] = rev_id
+            scores_list.append(score)
+            damaging_score = -1
+            damaging_prediction = ""
+            if 'damaging' in score and 'score' in score['damaging']:
+                damaging_score = score['damaging']['score']['probability']['true']
+                damaging_prediction = str(score['damaging']['score']['prediction'])
+            goodfaith_score = -1
+            goodfaith_prediction = ""
+            if 'goodfaith' in score and 'score' in score['goodfaith']:
+                goodfaith_score = score['goodfaith']['score']['probability']['true']
+                goodfaith_prediction = str(score['goodfaith']['score']['prediction'])
+            outfile.write(f"{rev_id},{damaging_score},{damaging_prediction},{goodfaith_score},{goodfaith_prediction}\n")
+            total_processed += 1
+    
+    # save out the scores list so it can be inspected
+    scores_pickle_filepath = os.path.join(working_dir, 'sample3_scores_list.pkl')
+    with open(scores_pickle_filepath, 'wb') as outfile:
+        pickle.dump(scores_list, outfile)
+        
+    print(f"Processed {total_processed} ORES scores.")
+        
+        
+if __name__ == "__main__":
+    main()
