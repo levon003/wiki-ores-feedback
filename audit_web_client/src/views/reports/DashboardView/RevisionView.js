d4904b9e26f0cfc8eafa06d58f240d180c5a00bb
phyllisgan <ganxn99@gmail.com>
2021-03-03 09:52:29 -0600

    ammend compare api

--- audit_web_client/src/views/reports/DashboardView/RevisionView.js
+++ audit_web_client/src/views/reports/DashboardView/RevisionView.js
@@ -51,11 +51,16 @@ const RevisionView = ({revision, className, ...rest }) => {
       })
         .then(res => res.json())
         .then(data => {
-          //console.log(data.compare['*']);
+          console.log(data.compare['*']);
           setRevisionDiff(data.compare['*']);
     });
   }, []);
-  
+
+  // <Box maxHeight="20vh" maxWidth="80vw" style={{'overflowY': 'scroll'}}>
+  //         <div dangerouslySetInnerHTML={{__html: revisionDiff}} />
+  //       </Box>
+  // take in string.. 
+
   return (
     <Paper
       className={clsx(classes.root, className)}
@@ -71,111 +76,16 @@ const RevisionView = ({revision, className, ...rest }) => {
         flexGrow={1}
         width = "1300px"
       >
-        {/* style={{'overflowY': 'scroll'}} */}
-        {/* ext-overflow= ellipsis; flexWrap="wrap" */}
-        {/* 'text-overflow': 'ellipsis', 'white-space': 'nowrap', 'overflow': '' */}
-        {/* marginRight='20px' flexWrap="wrap"  */}
-
         <Box maxHeight="80vh" width="80vx" style={{'overflowY': 'scroll'}}>
-        <tr>
-            <td colspan="2" class="diff-lineno">Line 33:</td>
-          </tr>
-          <tr>
-            <td class="diff-marker">&#160;</td>
-            <td class="diff-context"></td>
-          </tr>
-          <tr>
-            <td class="diff-marker">&#160;</td>
-            <td class="diff-context"><div>==Description==</div></td>
-          </tr>
-          <tr>
-            <td class="diff-marker">−</td>
-            <td class="diff-deletedline">
-               <div>
-                {`''Armadillidium vulgare'' may reach a length of {{convert|18|mm}}, and is capable of rolling into a ball when disturbed; this ability, along with its general appearance, gives it the name ''pill-bug''<del class="diffchange diffchange-inline"> or "Rolly Polly"</del> and also creates the potential for confusion with [[pill millipede]]s such as ''[[Glomeris marginata]]''.&lt;ref name="ARKive"&gt;{{cite web |url=http://www.arkive.org/pill-woodlouse/armadillidium-vulgare/info.html |title=Pill woodlouse (''Armadillidium vulgare'') |publisher=[[ARKive|ARKive.org]] |accessdate=February 13, 2009 |archive-url=https://web.archive.org/web/20090903034429/http://www.arkive.org/pill-woodlouse/armadillidium-vulgare/info.html |archive-date=2009-09-03 |url-status=dead }}&lt;/ref&gt; It can be distinguished from ''[[Armadillidium nasatum]]'' and ''[[Armadillidium depressum]]'' by the gap that ''A. nasatum'' and ''A. depressum'' leave when rolling into a ball; ''A. vulgare'' does not leave such a gap.&lt;ref&gt;{{cite web |url=http://www.nhm.ac.uk/nature-online/life/other-invertebrates/walking-with-woodlice/identification.html |title=Woodlouse Wizard: an identification key |publisher=[[Natural History Museum, London|Natural History Museum]] |accessdate=August 20, 2014}}&lt;/ref&gt;`}
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td class="diff-marker">&#160;</td>
-            <td class="diff-context"></td>
-          </tr>
-          <tr>
-            <td class="diff-marker">&#160;</td>
-            <td class="diff-context"><div>==Ecology==</div></td>
-          </tr>
-          <tr>
-            <td colspan="2" class="diff-lineno">Line 49:</td>
-          </tr>
-          <tr>
-            <td class="diff-marker">&#160;</td>
-            <td class="diff-context"></td>
-          </tr>
-          <tr>
-            <td class="diff-marker">&#160;</td>
-            <td class="diff-context"><div>==Mitochondrial genome==</div></td>
-          </tr>
-          <tr>
-            <td class="diff-marker">−</td>
-            <td class="diff-deletedline"><div>{`Most metazoans have circular [[Mitochondrial DNA|mitochondrial genomes]], but ''A.vulgare'' has an unusual combination of both circular and linear mitochondrial DNA.&lt;ref&gt;{{cite journal|doi=10.1007/s00239-007-9037-5|pmid=17906827|title=Structure and Evolution of the Atypical Mitochondrial Genome of Armadillidium vulgare (Isopoda, Crustacea)|journal=Journal of Molecular Evolution|volume=65|issue=6|pages=651–9|year=2007|last1=Marcadé|first1=Isabelle|last2=Cordaux|first2=Richard|last3=Doublet|first3=Vincent|last4=Debenest|first4=Catherine|last5=Bouchon|first5=Didier|last6=Raimond|first6=Roland|bibcode=2007JMolE..65..651M|citeseerx=10.1.1.688.9796}}&lt;/ref&gt;`}<del class="diffchange diffchange-inline"> They often also live underground so.. if you go digging next time make sure you are exty careful!</del></div></td>
-          </tr>
-          <tr>
-            <td class="diff-marker">&#160;</td>
-            <td class="diff-context"></td>
-          </tr>
-          <tr>
-            <td class="diff-marker">&#160;</td>
-            <td class="diff-context"><div>==Gallery==</div></td>
-          </tr>
-        </Box>
-        
-        <Box maxHeight="80vh" maxWidth="80vw" style={{'overflowY': 'scroll'}}>
-        <tr>
-          <td colspan="2" class="diff-lineno">Line 33:</td>
-        </tr>
-        <tr>
-          <td class="diff-marker">&#160;</td>
-          <td class="diff-context"></td>
-        </tr>
-        <tr>
-          <td class="diff-marker">&#160;</td>
-          <td class="diff-context"><div>==Description==</div></td>
-        </tr>
-        <tr>
-          <td class="diff-marker">+</td>
-          <td class="diff-addedline"><div>{`''Armadillidium vulgare'' may reach a length of {{convert|18|mm}}, and is capable of rolling into a ball when disturbed; this ability, along with its general appearance, gives it the name ''pill-bug'' and also creates the potential for confusion with [[pill millipede]]s such as ''[[Glomeris marginata]]''.&lt;ref name="ARKive"&gt;{{cite web |url=http://www.arkive.org/pill-woodlouse/armadillidium-vulgare/info.html |title=Pill woodlouse (''Armadillidium vulgare'') |publisher=[[ARKive|ARKive.org]] |accessdate=February 13, 2009 |archive-url=https://web.archive.org/web/20090903034429/http://www.arkive.org/pill-woodlouse/armadillidium-vulgare/info.html |archive-date=2009-09-03 |url-status=dead }}&lt;/ref&gt; It can be distinguished from ''[[Armadillidium nasatum]]'' and ''[[Armadillidium depressum]]'' by the gap that ''A. nasatum'' and ''A. depressum'' leave when rolling into a ball; ''A. vulgare'' does not leave such a gap.&lt;ref&gt;{{cite web |url=http://www.nhm.ac.uk/nature-online/life/other-invertebrates/walking-with-woodlice/identification.html |title=Woodlouse Wizard: an identification key |publisher=[[Natural History Museum, London|Natural History Museum]] |accessdate=August 20, 2014}}&lt;/ref&gt;`}</div></td>
-        </tr>
-        <tr>
-          <td class="diff-marker">&#160;</td>
-          <td class="diff-context"></td>
-        </tr>
-        <tr>
-          <td class="diff-marker">&#160;</td>
-          <td class="diff-context"><div>==Ecology==</div></td>
-        </tr>
-        <tr>
-          <td colspan="2" class="diff-lineno">Line 49:</td>
-        </tr>
-        <tr>
-          <td class="diff-marker">&#160;</td>
-          <td class="diff-context"></td>
-        </tr>
-        <tr>
-          <td class="diff-marker">&#160;</td>
-          <td class="diff-context"><div>==Mitochondrial genome==</div></td>
-        </tr>
-        <tr>
-          <td class="diff-marker">+</td>
-          <td class="diff-addedline"><div>{`Most metazoans have circular [[Mitochondrial DNA|mitochondrial genomes]], but ''A.vulgare'' has an unusual combination of both circular and linear mitochondrial DNA.&lt;ref&gt;{{cite journal|doi=10.1007/s00239-007-9037-5|pmid=17906827|title=Structure and Evolution of the Atypical Mitochondrial Genome of Armadillidium vulgare (Isopoda, Crustacea)|journal=Journal of Molecular Evolution|volume=65|issue=6|pages=651–9|year=2007|last1=Marcadé|first1=Isabelle|last2=Cordaux|first2=Richard|last3=Doublet|first3=Vincent|last4=Debenest|first4=Catherine|last5=Bouchon|first5=Didier|last6=Raimond|first6=Roland|bibcode=2007JMolE..65..651M|citeseerx=10.1.1.688.9796}}&lt;/ref&gt;`}</div></td>
-        </tr>
-        <tr>
-          <td class="diff-marker">&#160;</td>
-          <td class="diff-context"></td>
-        </tr>
-        <tr>
-          <td class="diff-marker">&#160;</td>
-          <td class="diff-context"><div>==Gallery==</div></td>
-        </tr>
+        <table class="diff diff-contentalign-left diff-editfont-monospace" data-mw="interface">
+        <colgroup>
+        <col class="diff-marker"/>
+				<col class="diff-content"/>
+				<col class="diff-marker"/>
+				<col class="diff-content"/>
+				</colgroup> 
+      <tbody dangerouslySetInnerHTML={{__html: revisionDiff}}></tbody>
+        </table>
         </Box>
       </Box>
     </Paper>
