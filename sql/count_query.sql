eca3de1ecc994d1673cf555f3bcff9f0e4590e92
Zachary Levonian <zwlevonian@gmail.com>
2021-04-28 00:09:07 -0500

    Added additional query experiments, and added index file to keep track of virtual columns and created indices. Still working out some problems.

--- sql/count_query.sql
+++ sql/count_query.sql
@@ -1,5 +1,112 @@
 DESCRIBE revision;
 DESCRIBE page;
+SELECT 0 FROM nonexistent;
+
+SELECT * FROM information_schema.tables WHERE table_schema = DATABASE();
+
+# Print summary info about total memory usage of OIDB
+SELECT t.table_schema, t.table_name, 
+t.data_length/1024/1024/1024 AS table_size_in_gb, 
+t.index_length/1024/1024/1024 AS index_size_in_gb, 
+(t.data_length + t.index_length)/1024/1024/1024 AS total_size_in_gb FROM
+(SELECT * FROM information_schema.tables WHERE table_schema = DATABASE()) AS t;
+
+
+###################
+SELECT rev_id, page_id, rev_timestamp, seconds_to_prev, is_minor, user_text, is_user_registered
+FROM revision 
+WHERE page_namespace = 0 AND page_rev_count = 3 AND is_user_bot = 0
+AND damaging_pred_filter = 2 AND is_reverted = 1 AND is_reverted_for_damage = 1
+ORDER BY random
+LIMIT 0,20;
+
+SELECT rev_id, page_id, rev_timestamp, seconds_to_prev, is_minor, user_text, is_user_registered
+FROM revision 
+WHERE page_namespace = 1 AND is_user_bot = 0
+AND damaging_pred_filter = 2 AND is_reverted = 1 AND is_reverted_for_damage = 1
+ORDER BY random
+LIMIT 0,20;
+
+SELECT COUNT(*)
+FROM revision 
+WHERE page_namespace = 0 AND page_rev_count = 3 AND is_user_bot = 0
+AND damaging_pred_filter = 2 AND is_reverted = 1 AND is_reverted_for_damage = 1;
+
+SELECT page_namespace, COUNT(*)
+FROM revision
+GROUP BY page_namespace;
+
+SELECT page_namespace, is_user_bot, damaging_pred_filter, is_reverted, is_reverted_for_damage, COUNT(*)
+FROM revision
+WHERE is_reverted = is_reverted_for_damage
+GROUP BY page_namespace, is_user_bot, damaging_pred_filter, is_reverted, is_reverted_for_damage;
+
+
+###################
+# Analysis of ClueBot NG, who has user_id = 13286072
+
+# What is the time to revert for ClueBot NG?
+# Note: this requires a join, since seconds_to_revert is a property of the reverted revisions, not the revert itself
+SELECT LEAST(revision.seconds_to_revert, 30) AS seconds_to_revert, COUNT(*) as reverted_rev_count
+FROM revision
+LEFT JOIN revision AS revert ON revision.revert_id = revert.rev_id
+WHERE revision.is_reverted = 1 AND revert.user_id = 13286072
+GROUP BY LEAST(revision.seconds_to_revert, 30);
+/*
+Seconds_to_revert, reverted_rev_count
+0, 4
+1, 9501
+2, 28877
+3, 18220
+4, 10886
+5, 8009
+6, 4857
+7, 2905
+8, 1717
+9, 1054
+10, 709
+11, 514
+12, 348
+13, 266
+14, 173
+15, 149
+16, 122
+17, 113
+18, 92
+19, 93
+20, 94
+21, 77
+22, 51
+23, 78
+24, 82
+25, 49
+26, 59
+27, 65
+28, 69
+29, 72
+30, 16980
+
+*/
+
+SELECT revert.user_text as reverting_user_text, revert.rev_id as reverting_rev_id, revision.seconds_to_revert, revision.*
+FROM revision
+LEFT JOIN revision AS revert ON revision.revert_id = revert.rev_id
+WHERE revision.is_reverted = 1 AND revision.seconds_to_revert >= 30 AND revert.user_id = 13286072
+LIMIT 10;
+
+SELECT COUNT(*), SUM(is_revert) FROM revision WHERE page_namespace = 0 AND user_id = 13286072;  # ns0 revisions by ClueBot NG; 93523 total, 93245 reverts
+SELECT * FROM revision WHERE page_namespace = 0 AND user_id = 13286072 AND is_revert = 0;  # investigate the non-reverts about the non-reverts?
+
+###################
+# Analysis of the length of page titles
+SELECT MAX(LENGTH(page_title)) FROM page;
+SELECT page_title, LENGTH(page_title) FROM page ORDER BY LENGTH(page_title) DESC LIMIT 0, 10;
+SELECT COUNT(*) FROM page;
+SELECT page_title, LENGTH(page_title) FROM page ORDER BY LENGTH(page_title) DESC LIMIT 474449, 5;  # 474449 = 9489004 × 0.05, aka the 95% of title lengths, returns 45
+SELECT page_title, LENGTH(page_title) FROM page ORDER BY LENGTH(page_title) DESC LIMIT 94890, 5;  # 99%
+SELECT page_title, LENGTH(page_title) FROM page ORDER BY LENGTH(page_title) DESC LIMIT 9489, 5;  # 99.9%, returns 83
+
+###################
 
 SELECT * FROM page 
 WHERE wiki_namespace = 0
@@ -12,7 +119,6 @@ LIMIT 100;
 SELECT * FROM revision WHERE rev_id = 880062021;
 SELECT * FROM revision WHERE is_reverted = 1 LIMIT 10;
 
-SELECT 0 FROM nonexistent;
 
 DROP INDEX revision_user_text ON revision;
 CREATE INDEX revision_user_text ON revision (user_text(80) );
@@ -83,10 +189,10 @@ damaging_pred <= 0.301  # LG
 For revert filtering, there's:
 is_reverted = 1  # required condition
 is_self_revert = X  # Boolean
-is_reverted_to_by_other  # Boolean
-is_user_trusted  # Boolean
-revert_set_size  # Multi-reversion reverts, 2 unique values in [1, 1+]
-seconds_to_revert  # Time to revert, 6 unique values in [6 seconds, 6 minutes, 6 hours, 6 days, 6 months, 1 year]
+is_reverted_to_by_other = X # Boolean
+is_user_trusted = X # Boolean
+revert_set_size = X # Multi-reversion reverts, 2 unique values in [1, 1+]
+seconds_to_revert = X # Time to revert, 6 unique values in [6 seconds, 6 minutes, 6 hours, 6 days, 6 months, 1 year]
 OR is_reverted = 0
 
 That's 3 * 2 * 2 * 2 * 2 * 6 + 1 = 289
@@ -147,10 +253,10 @@ WHERE wiki_namespace = 0 AND is_user_bot=0
 ORDER BY random
 LIMIT 100;
 
-CREATE INDEX revision_default ON revision (is_user_bot, random );
-CREATE INDEX revision_join_default ON revision (page_id, is_user_bot, random );
-CREATE INDEX page_default ON page (wiki_namespace, rev_count );
-CREATE INDEX page_join_default ON page (page_id, wiki_namespace, rev_count );
+CREATE INDEX revision_default ON revision (page_namespace, page_rev_count, is_user_bot, random );
+#CREATE INDEX revision_join_default ON revision (page_id, is_user_bot, random );
+#CREATE INDEX page_default ON page (wiki_namespace, rev_count );
+#CREATE INDEX page_join_default ON page (page_id, wiki_namespace, rev_count );
 
 CREATE INDEX revision_count ON revision (is_user_bot, damaging_pred, seconds_to_revert );
 
---
+++ sql/index.sql
@@ -0,0 +1,131 @@
+# Create persistent virtual columns
+ALTER TABLE revision
+ADD COLUMN damaging_pred_filter BOOLEAN AS (CASE WHEN damaging_pred >= 0.944 THEN 2 WHEN damaging_pred <= 0.301 THEN 0 ELSE 1 END) PERSISTENT,
+ADD COLUMN is_reverted_for_damage BOOLEAN AS (is_reverted AND NOT (is_reverted_to_by_other OR is_self_reverted)) PERSISTENT;
+
+ALTER TABLE revision
+ADD COLUMN delta_bytes_filter TINYINT(1) SIGNED AS (CASE 
+	WHEN delta_bytes IS NULL THEN NULL
+	WHEN delta_bytes >= 1000 THEN 2 
+    WHEN delta_bytes >= 20 THEN 1
+    WHEN delta_bytes > -20 THEN 0
+    WHEN delta_bytes > -1000 THEN -1
+    WHEN delta_bytes <= -1000 THEN -2
+    ELSE NULL
+END) PERSISTENT,
+ADD COLUMN rev_count_gt_filter TINYINT(1) UNSIGNED AS (CASE 
+	WHEN page_rev_count >= 100 THEN 5
+    WHEN page_rev_count >= 10 THEN 4
+    WHEN page_rev_count >= 5 THEN 3
+    WHEN page_rev_count >= 3 THEN 2
+    WHEN page_rev_count >= 2 THEN 1
+    ELSE 0
+END) PERSISTENT,
+ADD COLUMN rev_count_lt_filter TINYINT(1) UNSIGNED AS (CASE 
+	WHEN page_rev_count <= 1 THEN 0
+    WHEN page_rev_count <= 2 THEN 1
+    WHEN page_rev_count <= 3 THEN 2
+    WHEN page_rev_count <= 5 THEN 3
+    WHEN page_rev_count <= 10 THEN 4
+    ELSE 5
+END) PERSISTENT;
+
+ALTER TABLE revision
+ADD COLUMN str INT(11) AS (seconds_to_revert) VIRTUAL;
+
+ALTER TABLE revision
+ADD COLUMN reverted_within_filter TINYINT(1) UNSIGNED AS (CASE 
+	WHEN str IS NULL THEN NULL  # null when not set
+	WHEN str <= 6 THEN 0  # 6 seconds
+    WHEN str <= 360 THEN 1  # 6 minutes
+    WHEN str <= 21600 THEN 2  # 6 hours
+    WHEN str <= 518400 THEN 3  # 6 days
+    WHEN str <= 15552000 THEN 4  # 6 months
+    ELSE 5  # 1 year
+END) PERSISTENT,
+ADD COLUMN reverted_after_filter TINYINT(1) UNSIGNED AS (CASE 
+	WHEN str IS NULL THEN NULL  # null when not set
+	WHEN str >= 15552000 THEN 5  # reverted after more than 6 months
+    WHEN str >= 518400 THEN 4  # 6 days
+    WHEN str >= 21600 THEN 3  # 6 hours
+    WHEN str >= 360 THEN 2  # 6 minutes
+    WHEN str >= 6 THEN 1  # 6 seconds
+    ELSE 0  # reverted after less than five seconds
+END) PERSISTENT;
+
+ALTER TABLE revision 
+ADD COLUMN reverted_within_filter TINYINT(1) UNSIGNED,
+ADD COLUMN reverted_after_filter TINYINT(1) UNSIGNED;
+# This is the "compute it myself" solution to the 252-character limitation imposed on ADD COLUMN statements that add virtual columns.
+# Error Code: 1470. String '...' is too long for VIRTUAL COLUMN EXPRESSION (should be no longer than 252)
+UPDATE revision SET reverted_within_filter = (CASE 
+	WHEN seconds_to_revert IS NULL THEN NULL  # null when not set
+	WHEN seconds_to_revert <= 6 THEN 0  # 6 seconds
+    WHEN seconds_to_revert <= 6*60 THEN 1  # 6 minutes
+    WHEN seconds_to_revert <= 6*60*60 THEN 2  # 6 hours
+    WHEN seconds_to_revert <= 6*60*60*24 THEN 3  # 6 days
+    WHEN seconds_to_revert <= 6*60*60*24*30 THEN 4  # 6 months
+    ELSE 5  # 1 year
+END);
+
+/*
+reverted_within_filter / reverted_after_filter and rev_count_gt_filter / rev_count_lt_filter
+both have a weird relationship.
+
+For counts/filtering, it doesn't make sense to consider all combinations; only combinations that produce valid predicates that could return at least 1 row.
+(This is important because all 6*6 combinations is a lot, but far fewer should actually be considered valid filter combinations.)
+specifically, when:
+rev_count_gt_filter = 1 (rev_count >= 2) then:
+	when rev_count_lt_filter = 1 (rev_count <= 2) just remove all filters on this column
+    when rev_count_lt_filter = 0 (rev_count <= 1) this is an inverted and thus invalid equality
+So when generating counts, use condition rev_count_lt_filter <= rev_count_gt_filter
+
+For reverted_within_filter / reverted_after_filter, 
+use condition reverted_within_filter <= reverted_after_filter (- 1?)
+
+ADD COLUMN delta_bytes_filter TINYINT(1) SIGNED AS (CASE 
+	WHEN delta_bytes IS NULL THEN NULL
+	WHEN delta_bytes >= 1000 THEN 2 
+    WHEN delta_bytes < 1000 AND delta_bytes >= 20 THEN 1
+    WHEN delta_bytes < 20 AND delta_bytes > -20 THEN 0
+    WHEN delta_bytes > -1000 AND delta_bytes <= -20 THEN -1
+    WHEN delta_bytes <= -1000 THEN -2
+    ELSE NULL
+END) PERSISTENT
+ADD COLUMN reverted_within_filter TINYINT(1) UNSIGNED AS (CASE 
+	WHEN seconds_to_revert IS NULL THEN NULL  # null when not set
+	WHEN seconds_to_revert <= 6 THEN 0  # 6 seconds
+    WHEN seconds_to_revert <= 6*60 THEN 1  # 6 minutes
+    WHEN seconds_to_revert <= 6*60*60 THEN 2  # 6 hours
+    WHEN seconds_to_revert <= 6*60*60*24 THEN 3  # 6 days
+    WHEN seconds_to_revert <= 6*60*60*24*30 THEN 4  # 6 months
+    ELSE 5  # 1 year
+END) PERSISTENT,
+ADD COLUMN reverted_after_filter TINYINT(1) UNSIGNED AS (CASE 
+	WHEN seconds_to_revert IS NULL THEN NULL  # null when not set
+	WHEN seconds_to_revert >= 6*60*60*24*30 THEN 5  # reverted after more than 6 months
+    WHEN seconds_to_revert >= 6*60*60*24 THEN 4  # 6 days
+    WHEN seconds_to_revert >= 6*60*60 THEN 3  # 6 hours
+    WHEN seconds_to_revert >= 6*60 THEN 2  # 6 minutes
+    WHEN seconds_to_revert >= 6 THEN 1  # 6 seconds
+    ELSE 0  # reverted after less than five seconds
+END) PERSISTENT;
+
+*/
+
+#ALTER TABLE revision MODIFY COLUMN damaging_pred_filter TINYINT AS (CASE WHEN damaging_pred >= 0.944 THEN 2 WHEN damaging_pred <= 0.301 THEN 0 ELSE 1 END) PERSISTENT;
+
+SELECT * FROM revision;
+SELECT * FROM revision WHERE damaging_pred_filter = 2;
+
+# Create revision indices
+CREATE INDEX revision_default ON revision(page_rev_count, page_namespace, is_user_bot, damaging_pred_filter, is_reverted, is_reverted_for_damage, random);
+CREATE INDEX revision_default_random ON revision(random, page_rev_count, page_namespace, is_user_bot, damaging_pred_filter, is_reverted, is_reverted_for_damage);
+
+
+DROP INDEX revision_default ON revision;
+
+SHOW TABLE STATUS;
+
+# Create page indices
+CREATE INDEX page_title_index ON page (page_title(83) );
