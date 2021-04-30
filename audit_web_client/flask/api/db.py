37824fad3864c1120af8ccb1a6b8d2a96ee97066
Zachary Levonian <zwlevonian@gmail.com>
2021-04-29 20:06:16 -0500

    Updated create-db to produce and include the generated columns.

--- audit_web_client/flask/api/db.py
+++ audit_web_client/flask/api/db.py
@@ -5,6 +5,7 @@ from flask.cli import with_appcontext
 import sqlalchemy as sa
 from sqlalchemy import create_engine, Table, Column, Integer, SmallInteger, String, MetaData, ForeignKey, Text, Boolean, Float, Index, bindparam
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.dialects.mysql import TINYINT
 
 import os
 import json
@@ -95,7 +96,7 @@ def get_metadata():
         Column('curr_bytes', Integer, nullable=False),
         Column('delta_bytes', Integer),
         Column('page_rev_count', Integer, nullable=False),
-        Column('page_namespace', SmallInteger, nullable=False),
+        Column('page_namespace', TINYINT, nullable=False), # max value: 101?
         Column('is_page_redirect', Boolean, nullable=False),
         
         Column('is_reverted', Boolean, nullable=False),
@@ -104,7 +105,7 @@ def get_metadata():
         Column('is_self_reverted', Boolean, nullable=False),
         Column('is_self_revert', Boolean, nullable=False),
         Column('revert_target_id', Integer),
-        Column('revert_set_size', Integer),
+        Column('revert_set_size', TINYINT),  # max value: 15
         Column('revert_id', Integer),
         Column('seconds_to_revert', Integer),
 
@@ -113,6 +114,20 @@ def get_metadata():
         Column('ores_model_version', Text(length=6), nullable=False),
           
         Column('random', Float, nullable=False),
+
+        # filter columns: derived entirely from other columns
+        # we could generate these as virtual/generated columns with PERSISTENT,
+        # but generating them here makes things a bit easier by keeping it in one place
+        # and not needing to call ALTER TABLE after the table is generated
+        Column('user_type', TINYINT, nullable=False),
+        Column('damaging_pred_filter', TINYINT, nullable=False),
+        Column('reverted_filter_mask', TINYINT, nullable=False),
+        Column('revision_filter_mask', TINYINT, nullable=False),
+        Column('delta_bytes_filter', TINYINT, nullable=True),
+        Column('rev_count_gt_filter', TINYINT, nullable=False),
+        Column('rev_count_lt_filter', TINYINT, nullable=False),
+        Column('reverted_within_filter', TINYINT, nullable=True),
+        Column('reverted_after_filter', TINYINT, nullable=True),
     )
     return g.oidb_metadata
 
@@ -252,7 +267,7 @@ def import_revision_data(engine, revision_table, data_dir, oidb_dir, rev_tsv_fil
                 #else:  # this is an unregistered user; we could these separately
                 #    user_text_editcount_dict[user_text] += 1
                 #    n_user_contributions_at_rev = user_text_editcount_dict[user_id]
-                is_user_bot = user_id is not None and user_id in registered_bot_user_ids
+                #is_user_bot = user_id is not None and user_id in registered_bot_user_ids
                 rev_data = {
                     'page_id': int(page_id),
                     'rev_id': int(rev_id), 
@@ -286,6 +301,7 @@ def import_revision_data(engine, revision_table, data_dir, oidb_dir, rev_tsv_fil
                     'ores_model_version': model_version,
                     'random': rng.random(),
                 }
+                add_derived_columns_to_rev_data(rev_data)
                 rev_list.append(rev_data)
 
                 if len(rev_list) >= INSERT_BATCH_SIZE:
@@ -296,6 +312,169 @@ def import_revision_data(engine, revision_table, data_dir, oidb_dir, rev_tsv_fil
                 conn.execute(revision_table.insert(), rev_list)
                 rev_list = []
             logger.debug(f"Final revision insert ({processed_count} total) complete in {datetime.now() - start}.")
+
+def add_derived_columns_to_rev_data(rev_data):
+    """
+    Generate derived columns from the given rev_data, adding them to the passed dictionary.
+
+    Generates the following columns / dict keys:
+        user_type
+        damaging_pred_filter
+        reverted_filter_mask
+        revision_filter_mask
+        delta_bytes_filter
+        rev_count_gt_filter
+        rev_count_lt_filter
+        reverted_within_filter
+        reverted_after_filter
+
+    Index is built on:
+        user_type
+        revision_filter_mask
+        delta_bytes_filter
+        page_namespace
+        rev_count_gt_filter
+        rev_count_lt_filter
+        damaging_pred_filter
+        reverted_filter_mask
+        reverted_within_filter
+        reverted_after_filter
+        random
+    
+    reverted_filter_mask = 1 byte mask of:
+        is_reverted
+        is_reverted_to_by_other
+        is_self_reverted
+        is_user_trusted
+
+    revision_filter_mask = 1 byte mask of:
+        is_revert
+        has_edit_summary
+        is_minor
+
+    """
+    user_type = 0
+    if rev_data['is_user_registered']:
+        if rev_data['is_user_bot']:
+            user_type = 1
+        else:
+            n_contribs = rev_data['n_user_contributions_at_rev']
+            if n_contribs < 10:
+                user_type = 2
+            elif n_contribs < 500:
+                user_type = 3
+            else:
+                user_type = 4
+    rev_data['user_type'] = user_type
+
+    if rev_data['damaging_pred'] >= 0.944:
+        damaging_pred_filter = 2
+    elif rev_data['damaging_pred'] <= 0.301:
+        damaging_pred_filter = 0
+    else:
+        damaging_pred_filter = 1
+    rev_data['damaging_pred_filter'] = damaging_pred_filter
+
+    reverted_filter_mask = 0
+    if rev_data['is_reverted']:
+        # build reverted_filter as a bit mask
+        reverted_filter_mask = 1
+        if rev_data['is_reverted_to_by_other']:
+            reverted_filter_mask |= (1<<1)
+        if rev_data['is_self_reverted']:
+            reverted_filter_mask |= (1<<2)
+        if rev_data['is_user_trusted']:
+            reverted_filter_mask |= (1<<3)
+    rev_data['reverted_filter_mask'] = reverted_filter_mask
+
+    revision_filter_mask = 0
+    if rev_data['is_revert']:
+        revision_filter_mask += 1
+    if rev_data['has_edit_summary']:
+        revision_filter_mask += 2
+    if rev_data['is_minor']:
+        revision_filter_mask += 4
+    rev_data['revision_filter_mask'] = revision_filter_mask
+
+    delta_bytes_filter = None
+    if rev_data['delta_bytes'] is not None:
+        if rev_data['delta_bytes'] >= 1000:
+            delta_bytes_filter = 2
+        elif rev_data['delta_bytes'] > 20:
+            delta_bytes_filter = 1
+        elif rev_data['delta_bytes'] >= -20:
+            delta_bytes_filter = 0
+        elif rev_data['delta_bytes'] < -20:
+            delta_bytes_filter = -2
+        elif rev_data['delta_bytes'] <= -1000:
+            delta_bytes_filter = -2
+        else:
+            raise ValueError("Implementation error: coverage should be total")
+    rev_data['delta_bytes_filter'] = delta_bytes_filter
+
+
+    rc = rev_data['page_rev_count']
+    if rc >= 100:
+        rev_count_gt_filter = 5
+    elif rc >= 10:
+        rev_count_gt_filter = 4
+    elif rc >= 5:
+        rev_count_gt_filter = 3
+    elif rc >= 3:
+        rev_count_gt_filter = 2
+    elif rc >= 2:
+        rev_count_gt_filter = 1
+    else:
+        rev_count_gt_filter = 0
+    if rc <= 1:
+        rev_count_lt_filter = 0
+    elif rc <= 2:
+        rev_count_lt_filter = 1
+    elif rc <= 3:
+        rev_count_lt_filter = 2
+    elif rc <= 5:
+        rev_count_lt_filter = 3
+    elif rc <= 10:
+        rev_count_lt_filter = 4
+    else:
+        rev_count_lt_filter = 5
+    rev_data['rev_count_gt_filter'] = rev_count_gt_filter
+    rev_data['rev_count_lt_filter'] = rev_count_lt_filter
+
+    reverted_within_filter = None
+    reverted_after_filter = None
+    if rev_data['is_reverted']:
+        secs = rev_data['seconds_to_revert']
+        if secs <= 6:
+            reverted_within_filter = 0
+        elif secs <= 6*60:
+            reverted_within_filter = 1
+        elif secs <= 6*60*60:
+            reverted_within_filter = 2
+        elif secs <= 6*60*60*24:
+            reverted_within_filter = 3
+        elif secs <= 6*60*60*24*30:
+            reverted_within_filter = 4
+        else:
+            reverted_within_filter = 5
+
+        if secs >= 6*60*60*24*30:
+            reverted_after_filter = 5
+        elif secs <= 6*60*60*24:
+            reverted_after_filter = 4
+        elif secs <= 6*60*60:
+            reverted_after_filter = 3
+        elif secs <= 6*60:
+            reverted_after_filter = 2
+        elif secs <= 6:
+            reverted_after_filter = 1
+        else:
+            reverted_after_filter = 0
+    rev_data['reverted_within_filter'] = reverted_within_filter
+    rev_data['reverted_after_filter'] = reverted_after_filter
+
+
+
 
 
 @click.command('update-ores-scores')
