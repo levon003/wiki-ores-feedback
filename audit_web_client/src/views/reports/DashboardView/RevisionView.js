c7939c23e04e7040fa68eceef75c75edf265b211
LaurenHagen <56652938+LaurenHagen@users.noreply.github.com>
2022-04-24 13:34:26 -0500

    pushed previous/next buttons to the right side of screen

--- audit_web_client/src/views/reports/DashboardView/RevisionView.js
+++ audit_web_client/src/views/reports/DashboardView/RevisionView.js
@@ -788,89 +788,6 @@ const RevisionView = ({ revisions, setRevisions, className, currRevisionIdx, set
           <PredictionDisplay />
           <AnnotationButtons/>
 
-          {/* Notes */}
-          <Box
-              display="flex"
-              flexDirection="row"
-              style= {{ marginLeft: "24px" }}
-          >
-            <Box 
-              display="flex" 
-              style={{paddingTop: "8px", paddingBottom: "12%"}}
-            >
-                <TextField
-                multiline
-                name="noteTextField"
-                label="Notes" 
-                value={note} 
-                onChange={(event) => {
-                  setNote(event.target.value)
-                  setUnsentNoteUpdate(true)
-                  setTyping(true)
-                  setUserChangedNote(true)
-                  setNoteSuccess(null)
-                }} 
-                style={{width: "100%"}}
-                />
-                <NotesLoadingIcon typing={typing} userChangedNote={userChangedNote} noteSuccess={noteSuccess}/>
-            </Box>
-          </Box>
-          
-          {/* Previous/Next Buttons */}
-          <Box style={{display: "inline-flex" /*marginLeft: "auto", float: "right"*/}}>
-            {/* Previous Unannotated */}
-            <Box className="text-h4"
-            display="flex"
-            alignItems= "center"
-            justifyContent= "center"
-            title="Shortcut: z"
-            style={{cursor: 'pointer'}}
-            >
-              <Button disabled={currRevisionIdx === 0 || prevUnannotatedDisabledCount === -1} className="text-h4" onClick={(handlePreviousUnannotatedClick)}>
-                <ArrowBackIosIcon style={{marginRight: "4px", color: (currRevisionIdx === 0 || prevUnannotatedDisabledCount === -1) ? "#BDBDBD" : "black"}} className="text-h4"/>Previous Unannotated
-              </Button>
-            </Box>
-
-            {/* Previous */}
-            <Box className="text-h4"
-            display="flex"
-            alignItems= "center"
-            justifyContent= "center"
-            title="Shortcut: <left arrow>"
-            style={{marginLeft: "5px", cursor: 'pointer'}}
-            >
-              <Button disabled={currRevisionIdx === 0} className="text-h4" onClick={(handlePreviousClick)}>
-                <ArrowBackIcon style={{marginRight: "4px", color: (currRevisionIdx === 0) ? "#BDBDBD" : "black"}} className="text-h4"/>Previous
-              </Button>
-            </Box>
-
-            {/* Next */}
-            <Box 
-            display="flex"
-            alignItems= "center"
-            justifyContent= "center"
-            className="text-h4" 
-            title="Shortcut: <right arrow>"
-            style={{marginLeft: "5px", cursor: 'pointer'}}>
-              <Button disabled={currRevisionIdx === revisions.length - 1} className="text-h4" onClick={(handleNextClick)}>
-                Next<ArrowForwardIcon style={{marginLeft: "4px", color: (currRevisionIdx === revisions.length - 1) ? "#BDBDBD" : "black"}} className="text-h4"/>
-              </Button>
-            </Box>
-
-            {/* Next Unannotated */}
-            <Box 
-            display="flex"
-            alignItems= "center"
-            justifyContent= "center"
-            className="text-h4" 
-            title="Shortcut: x"
-            style={{marginLeft: "5px", cursor: 'pointer'}}>
-              <Button disabled={(currRevisionIdx === revisions.length - 1) || (nextUnannotatedDisabledCount === revisions.length)} className="text-h4" onClick={(handleNextUnannotatedClick)}>
-                  Next Unannotated<ArrowForwardIosIcon style={{marginLeft: "4px", color: ((currRevisionIdx === revisions.length - 1) || (nextUnannotatedDisabledCount === revisions.length)) ? "#BDBDBD" : "black"}} className="text-h4"/>
-              </Button>
-            </Box>
-          </Box>
-          
         </Box>  
     );
   }
@@ -966,6 +883,93 @@ const RevisionView = ({ revisions, setRevisions, className, currRevisionIdx, set
           <Box style={{marginRight: "8px"}}>
               <RevisionAnnotationControls/>
           </Box>
+
+          <Box>
+            {/* Notes */}
+            <Box
+                display="flex"
+                flexDirection="row"
+                style= {{ marginLeft: "24px" }}
+            >
+              <Box 
+                display="flex" 
+                style={{paddingTop: "8px", paddingBottom: "12%"}}
+              >
+                  <TextField
+                  multiline
+                  name="noteTextField"
+                  label="Notes" 
+                  value={note} 
+                  onChange={(event) => {
+                    setNote(event.target.value)
+                    setUnsentNoteUpdate(true)
+                    setTyping(true)
+                    setUserChangedNote(true)
+                    setNoteSuccess(null)
+                  }} 
+                  style={{width: "100%"}}
+                  />
+                  <NotesLoadingIcon typing={typing} userChangedNote={userChangedNote} noteSuccess={noteSuccess}/>
+              </Box>
+            </Box>
+          </Box>
+          
+          <Box style={{marginLeft: "auto"}}>
+            {/* Previous/Next Buttons */}
+            <Box style={{display: "inline-flex"}}>
+              {/* Previous Unannotated */}
+              <Box className="text-h4"
+              display="flex"
+              alignItems= "center"
+              justifyContent= "center"
+              title="Shortcut: z"
+              style={{cursor: 'pointer'}}
+              >
+                <Button disabled={currRevisionIdx === 0 || prevUnannotatedDisabledCount === -1} className="text-h4" onClick={(handlePreviousUnannotatedClick)}>
+                  <ArrowBackIosIcon style={{marginRight: "4px", color: (currRevisionIdx === 0 || prevUnannotatedDisabledCount === -1) ? "#BDBDBD" : "black"}} className="text-h4"/>Previous Unannotated
+                </Button>
+              </Box>
+
+              {/* Previous */}
+              <Box className="text-h4"
+              display="flex"
+              alignItems= "center"
+              justifyContent= "center"
+              title="Shortcut: <left arrow>"
+              style={{marginLeft: "5px", cursor: 'pointer'}}
+              >
+                <Button disabled={currRevisionIdx === 0} className="text-h4" onClick={(handlePreviousClick)}>
+                  <ArrowBackIcon style={{marginRight: "4px", color: (currRevisionIdx === 0) ? "#BDBDBD" : "black"}} className="text-h4"/>Previous
+                </Button>
+              </Box>
+
+              {/* Next */}
+              <Box 
+              display="flex"
+              alignItems= "center"
+              justifyContent= "center"
+              className="text-h4" 
+              title="Shortcut: <right arrow>"
+              style={{marginLeft: "5px", cursor: 'pointer'}}>
+                <Button disabled={currRevisionIdx === revisions.length - 1} className="text-h4" onClick={(handleNextClick)}>
+                  Next<ArrowForwardIcon style={{marginLeft: "4px", color: (currRevisionIdx === revisions.length - 1) ? "#BDBDBD" : "black"}} className="text-h4"/>
+                </Button>
+              </Box>
+
+              {/* Next Unannotated */}
+              <Box 
+              display="flex"
+              alignItems= "center"
+              justifyContent= "center"
+              className="text-h4" 
+              title="Shortcut: x"
+              style={{marginLeft: "5px", cursor: 'pointer'}}>
+                <Button disabled={(currRevisionIdx === revisions.length - 1) || (nextUnannotatedDisabledCount === revisions.length)} className="text-h4" onClick={(handleNextUnannotatedClick)}>
+                    Next Unannotated<ArrowForwardIosIcon style={{marginLeft: "4px", color: ((currRevisionIdx === revisions.length - 1) || (nextUnannotatedDisabledCount === revisions.length)) ? "#BDBDBD" : "black"}} className="text-h4"/>
+                </Button>
+              </Box>
+            </Box>
+          </Box>
           <ButtonLoadingIcon buttonSuccess={buttonSuccess}/>
 
       </Box>
@@ -973,8 +977,6 @@ const RevisionView = ({ revisions, setRevisions, className, currRevisionIdx, set
       <Box 
           style={{overflow: "auto", marginBottom: "5px"}}
       >
-
-
         {/* Article Number & Buttons */}
         <Box
             display="flex"
@@ -986,10 +988,6 @@ const RevisionView = ({ revisions, setRevisions, className, currRevisionIdx, set
           {currRevisionIdx === revisions.length - 1 && (
             <Box>You've reached the last revision for this set of filter criteria. Change the filters to get some new revisions.</Box>
           )}
-
-          
-
-          
         </Box>
       </Box>
     </Box>
